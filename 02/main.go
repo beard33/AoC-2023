d34c37645a636508a698b80b64304eb025d58a94
beard33 <andreaguzzon26@gmail.com>
2023-12-03 22:44:07 +0100

    Solve day 02 - 2

--- 02/main.go
+++ 02/main.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-var total int
+var total, total2 int
 
 var cubes = map[string]int{
 	"blue":  14,
@@ -22,15 +22,17 @@ func main() {
 		valid, id := isValidGame(str)
 		if valid {
 			numId, _ := strconv.Atoi(id)
-			fmt.Printf("ID: %v\n\n", id)
 			total += numId
 		}
+		total2 += setPower(str)
 	}
 	fmt.Printf("TOTAL: %v\n", total)
+	fmt.Printf("TOTAL 2: %v\n", total2)
 }
 
-func isValidGame(game string) (bool, string) {
+// PROBLEM 1
 
+func isValidGame(game string) (bool, string) {
 	splittedLine := strings.Split(game, ":")
 	gameId := strings.Split(splittedLine[0], " ")[1]
 	cubeDraws := strings.Split(splittedLine[1], ";")
@@ -51,12 +53,30 @@ func isValidDraw(draw string) bool {
 		count, _ := strconv.Atoi(spl[1])
 		color := spl[2]
 		if cubes[color] < count {
-			fmt.Printf("Count: %v Color: %v\n", count, color)
-			fmt.Println("Impossible!")
 			return false
 		}
 	}
-
 	return true
+}
 
+// PROBLEM 2
+
+func setPower(set string) int {
+
+	colorMap := make(map[string]int)
+	splittedLine := strings.Split(set, ":")
+	cubeDraws := strings.Split(splittedLine[1], ";")
+	for _, v := range cubeDraws {
+		splittedDraw := strings.Split(v, ",")
+		for _, v := range splittedDraw {
+			spl := strings.Split(v, " ")
+			count, _ := strconv.Atoi(spl[1])
+			color := spl[2]
+			if count > colorMap[color] {
+				colorMap[color] = count
+			}
+		}
+	}
+
+	return colorMap["green"] * colorMap["blue"] * colorMap["red"]
 }
